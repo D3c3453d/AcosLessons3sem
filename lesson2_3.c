ce72573ba538eff7e133a5096de0c51c716bfa31
D3c3453d <sergey.et02@gmail.com>
2022-09-30 14:33:15 +0300

    init

--- lesson2_3.c
+++ lesson2_3.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
     char *v[] = {"ls", "-lh", (char*) NULL};
-    execvp("ls", v);
+    execv("ls", v);
+
+    printf("%s\n", strerror(errno));
     return 0;
 }
